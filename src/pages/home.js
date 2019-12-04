ec3f3d4c1aa51c5e7a8fc50a840f627af07853ce
Abdi-01 <abdialghi@gmail.com>
2019-12-04 22:40:29 +0700

    Belajar membuat web bioskop
    Versi 0.1

---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+        <div>
+            <br/>
+            <br/>
+            <br/>
+            <h1>Ini Home</h1>
+        </div>
+        );
+    }
+}
+ 
+export default Home;
